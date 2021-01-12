e3d18a5f58490bb1fcc68bdd467c9f2f7a1e27d5
Jamie Corkhill <jamie.corkhill@live.com>
2021-01-12 13:25:50 -0600

    Add cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import { generateToken } from './api/controller';
 
 const PORT = parseInt(process.env.PORT as string) || 3000;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
